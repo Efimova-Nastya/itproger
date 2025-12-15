f6dc42a92211d8515d7999f54a5a7f134d43053b
Nastya Efimova <free.have.a.nice.day@gmail.com>
2025-12-15 14:06:08 +0300

    123

--- itproger/main/urls.py
+++ itproger/main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('comment/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
     path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('best/', views.book_detail, name='best'),
 ]
